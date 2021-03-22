19e0f6453de7d4800762cc2548888fcfca7f2c0d
BabochkinaPolina <79091702+BabochkinaPolina@users.noreply.github.com>
2021-03-22 14:08:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,24 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int rgran = size - 1, lgran = 0, kol = 0;
+    while (lgran < rgran) {
+        int center = (rgran + lgran) / 2;
+        if (arr[center] < value)
+            lgran = center + 1;
+        else
+            rgran = center;
+    }
+
+    if (arr[lgran] == value) {
+        while (arr[lgran] == value) {
+            kol += 1;
+            lgran += 1;
+        }
+    }
+
+    if (kol != 0) {
+        return kol;
+        else return 0; // если ничего не найдено
+    }
 }
